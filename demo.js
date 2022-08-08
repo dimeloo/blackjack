860e674cc8a47a1c8f85af2a45fbd82979b5c959
Mike Ortiz <dimelo@Mikes-MacBook-Air.local>
2022-08-08 00:59:09 -0400

    practice before blackjack game

---
+++ demo.js
@@ -0,0 +1,59 @@
+// Create two variables, firstName and lastName
+// Concatenate the two variables into a third variable called fullName
+// Log fullName to the console
+
+// const firstName = 'Javascript'
+// const lastName = 'Master'
+
+// const fullName = firstName + " " + lastName
+// console.log(fullName)
+
+// let name = 'Linda'
+// let greeting = 'Hi there'
+// let fullGreeting = greeting + ', ' + name + '!'
+
+// // Create a function that logs out "Hi there, Linda!" when called
+// function sendGreeting () {
+//     console.log(fullGreeting)
+// }
+// sendGreeting()
+/* ****************************************************************** */
+
+// let myPoints = 3
+
+// // Create two functions, add3Points() and remove1Point(), and have them
+// // add/remove points to/from the myPoints variable
+
+// function add3Points() {
+//   const points = myPoints + 3
+//   myPoints = points
+//   return myPoints
+// }
+
+// function remove1Point() {
+//   const points = myPoints - 1
+//   myPoints = points
+//   return myPoints
+// }
+
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+
+// // Call the functions to that the line below logs out 10
+// console.log(myPoints)
+/* ******************************************************************* */
+
+// Try to predict what each of the lines will log out
+// console.log("2" + 2) // 22
+// console.log(11 + 7) // 18
+// console.log(6 + "5") // 65
+// console.log("My points: " + 5 + 9) //My points: 59
+// console.log(2 + 2) // 4
+// console.log("11" + "14") // 1114
+
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
